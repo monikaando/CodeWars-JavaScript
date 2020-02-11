e3e70de709aa4ed5f46ee1c79ef543c470d044f5
Monika <mswidzinska87@gmail.com>
2020-02-11 16:09:06 +0100

    exercise

---
+++ Opposite-number.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Opposite number
+// Very simple, given a number, find its opposite.
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return -number;
+}
