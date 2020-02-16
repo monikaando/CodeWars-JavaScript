1f50620ab12b13766988b70bbd051055f781a291
Monika <mswidzinska87@gmail.com>
2020-02-16 18:04:49 +0100

    exercise

---
+++ The-Wide-Mouthed-frog.js
@@ -0,0 +1,22 @@
+// The wide mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But then he meet the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method take one argument animal which corresponds to the animal encountered by frog. If this one is an alligator (case insensitive) return small otherwise return wide.
+
+//Shorter solution
+function mouthSize(animal) {
+  return animal.toLowerCase() === "alligator" ? "small" : "wide";
+}
+
+// //Longer solution
+function mouthSize(animal) {
+  var creature = animal.toLowerCase();
+  if (creature === "alligator") {
+    return "small";
+  } else {
+    return "wide";
+  }
+}
