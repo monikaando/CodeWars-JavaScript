0eaa27c0be707e5631a59fb53d7c48bef0f1d188
Monika <mswidzinska87@gmail.com>
2020-02-18 10:56:26 +0100

    exercise

---
+++ Volume-of-a-Cuboid.js
@@ -0,0 +1,14 @@
+// 8 kyu
+// Volume of a Cuboid
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+// var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+})();
