e04c082b9dfb54af3a82662ad08d58e7148ed9fd
Monika <mswidzinska87@gmail.com>
2020-02-21 09:43:51 +0100

    exercise

---
+++ Total-amount-of-points.js
@@ -0,0 +1,30 @@
+// 8 kyu
+// Total amount of points
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let points = 0;
+  for (i = 0; i < games.length; i++) {
+    if (games[i][0] > games[i][2]) {
+      points += 3;
+    } else if (games[i][0] < games[i][2]) {
+      points += 0;
+    } else if ((games[i][0] = games[i][2])) {
+      points += 1;
+    }
+  }
+  return points;
+}
