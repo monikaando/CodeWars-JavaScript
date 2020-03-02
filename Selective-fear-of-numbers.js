6a190fc46e5def074ca29d128fe7beb98506592c
Monika <mswidzinska87@gmail.com>
2020-03-02 15:31:16 +0100

    exercise

--- Selective-fear-of-numbers.js
+++ Selective-fear-of-numbers.js
@@ -42,3 +42,21 @@ var AmIAfraid = function(day, num){
     return false;
   } 
 }
+
+//Shorter version
+var AmIAfraid = function(day, num){
+    if ((day=="Monday" && num == 12)||(day=="Tuesday" && num > 95)
+  ||(day=="Wednesday" && num == 34) || (day=="Thursday" && num == 0)
+  ||(day=="Friday" && num%2 == 0) || (day=="Saturday" && num == 56)
+  ||(day=="Sunday" && ((num == 666)||(num==-666) )))
+  {
+    return true
+  } else{
+    return false
+  }
+}
+//Short version
+  var AmIAfraid = function (day, num) {
+  return day == "Monday" ? num === 12 : day == "Tuesday" ? num > 95 : day == "Wednesday" ? num == 34 : day == "Thursday" ? num == 0 : day == "Friday" ? num % 2 == 0 : day == "Saturday" ? num == 56 : Math.abs(num) === 666;
+}
+
---
+++ Sum-of-Minimums.js
@@ -0,0 +1,24 @@
+// 7 kyu Sum of Minimums!
+// Given a 2D array of size m * n. Your task is to find the sum of minimum value in each row.
+
+// For Example:
+
+// [
+//   [1, 2, 3, 4, 5],       // minimum value of row is 1
+//   [5, 6, 7, 8, 9],       // minimum value of row is 5
+//   [20, 21, 34, 56, 100]  // minimum value of row is 20
+// ]
+// So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+// Note: You will be always given non-empty array containing Positive values.
+
+// ENJOY CODING :)
+
+function sumOfMinimums(arr) {
+  let sumRow = 0;
+     for (let i=0; i<arr.length; i++){
+       sumRow += Math.min.apply(Math,arr[i])
+      //  console.log(Math.min.apply(Math,arr[i]))
+  }
+return sumRow
+}
