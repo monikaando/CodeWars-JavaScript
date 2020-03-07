e5cbed2c506552df25ba33593a96448d9fb02128
Monika <mswidzinska87@gmail.com>
2020-03-07 18:56:31 +0100

    exercise

---
+++ Sum-The-Strings.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Sum The Strings
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+//   sumStr("4", "5")    // should output "9"
+//   sumStr("34", "5")   // should output "39"
+// If either input is an empty string, consider it as zero.
+
+function sumStr(a,b) {
+  var sum=(parseInt(a) + parseInt(b))
+  return sum.toString()
+}
