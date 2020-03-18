b896248a39469792c871a46764666bec5f35d127
Monika <mswidzinska87@gmail.com>
2020-03-18 15:02:51 +0100

    exercise

---
+++ Get-ascii-value-of-character.js
@@ -0,0 +1,9 @@
+// 8 kyu get ascii value of character
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+function getASCII(c){
+  return c.charCodeAt()
+}
+console.log(getASCII('A'))
+//65
